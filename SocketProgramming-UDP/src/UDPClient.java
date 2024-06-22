1766da5ac77a4b6adc9ffefdde559a6de9b99646
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-06-22 13:12:11 +0300

    The above code is for a UDP network.

---
+++ SocketProgramming-UDP/src/UDPClient.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPClient {
+
+    public static void main(String[] args) {
+        final String SERVER_ADDRESS = "localhost"; // Server's IP address or hostname
+        final int SERVER_PORT = 5555; // Server's port number
+
+        try (DatagramSocket clientSocket = new DatagramSocket()) {
+            InetAddress serverAddress = InetAddress.getByName(SERVER_ADDRESS);
+
+            // Message to send to the server
+            String message = "Hello from client";
+            byte[] sendData = message.getBytes();
+
+            // Create a DatagramPacket to send data to the server
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, SERVER_PORT);
+            clientSocket.send(sendPacket); // Send packet to server
+
+            // Prepare to receive response from the server
+            byte[] receiveData = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+            // Receive packet from server
+            clientSocket.receive(receivePacket);
+
+            // Process received data
+            String serverResponse = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            System.out.println("Server response: " + serverResponse);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ SocketProgramming-UDP/src/UDPServer.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPServer {
+
+    public static void main(String[] args) {
+        final int PORT = 5555; // Port number for server to bind
+
+        try (DatagramSocket serverSocket = new DatagramSocket(PORT)) {
+            System.out.println("Server is running and listening on port " + PORT);
+
+            while (true) {
+                byte[] receiveData = new byte[1024];
+
+                // Create a DatagramPacket to receive data from the client
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket); // Receive packet from client
+
+                // Extract client's IP address and port
+                InetAddress clientAddress = receivePacket.getAddress();
+                int clientPort = receivePacket.getPort();
+
+                // Process received data
+                String receivedMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                System.out.println("Received from client [" + clientAddress.getHostAddress() + ":" + clientPort + "]: " + receivedMessage);
+
+                // Prepare data to send back to the client
+                String serverResponse = "Hello from server";
+                byte[] sendData = serverResponse.getBytes();
+
+                // Create a DatagramPacket to send data to the client
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
+                serverSocket.send(sendPacket); // Send packet to client
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
